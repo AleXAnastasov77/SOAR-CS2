95d80cc58765f6a5609613fbe786d17ba983a44a
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-10-16 16:08:53 +0200

    allow all inbound from VPC for NAT

--- infra/network.tf
+++ infra/network.tf
@@ -121,6 +121,13 @@ resource "aws_security_group" "endpoints_sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16", "10.100.0.0/16"]
   }
+  ingress {
+    description = "Allow all traffic from private subnets (for NAT)"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
 
   egress {
     from_port   = 0
