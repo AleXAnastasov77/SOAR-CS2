42ec0545e441015d60b94edd17f49d4b2c799e00
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-10-16 16:36:51 +0200

    NAT gw test fix..

--- infra/nat_gw.sh
+++ infra/nat_gw.sh
@@ -4,7 +4,7 @@ echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 
 # Configure NAT (masquerade traffic from private subnets)
-iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o ens5 -j MASQUERADE
 
 # Save iptables
 apt update -y
