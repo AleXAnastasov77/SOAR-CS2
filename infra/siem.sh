496829d3451166c70a3fd4fd55578ba6160076d8
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-10-21 14:29:53 +0200

    downgrade versions to work with the wazuh plugin for elastic

--- infra/siem.sh
+++ infra/siem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 30
-/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic --batch > /root/elastic_pass.txt
-/usr/share/elasticsearch/bin/elasticsearch-reset-password -u kibana_system --batch >> /root/elastic_pass.txt
-aws ssm put-parameter --name "/soar/elastic/password" --type "SecureString" --overwrite --value "$(grep elastic_password /root/elastic_pass.txt | cut -d'=' -f2)"
+sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic --batch | sudo tee /root/elastic_pass.txt > /dev/null
+sudo /usr/share/elasticsearch/bin/elasticsearch-reset-password -u kibana_system --batch | sudo tee -a /root/elastic_pass.txt > /dev/null
+
