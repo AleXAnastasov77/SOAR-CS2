43db9f602948fbd386839e7dbee77378ce3d52d5
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-10-30 21:08:40 +0100

    infra update with the new ami and t3.large

--- infra/SOCTools.tf
+++ infra/SOCTools.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "SOC_Tools_instance" {
   ami                    = data.aws_ssm_parameter.soc_ami.value
-  instance_type          = "t3.medium"
+  instance_type          = "t3.large"
   vpc_security_group_ids = [aws_security_group.SOCTools_sg.id]
   subnet_id              = aws_subnet.privateSOCTools_cs2.id
   private_ip             = "10.0.12.10"
   key_name               = "ansible-keypair"
   tags = {
-    Name = "Security Tools Server"
+    Name = "SOC Tools Server"
   }
   root_block_device {
     encrypted   = true
